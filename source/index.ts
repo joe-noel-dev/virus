975934c4d8f0af52e86330b1f7cb6554d70f2c45
Joe Noel <joe.noel@focusrite.com>
2020-11-26 10:02:35 +0000

    Add mask

--- source/index.ts
+++ source/index.ts
@@ -12,6 +12,9 @@ interface Person {
   xVelocity: number;
   yVelocity: number;
 
+  mask: boolean;
+  isolating: boolean;
+
   state: State;
   infectionTime: number;
 }
@@ -23,11 +26,14 @@ interface World {
 
 const numPeople = 200;
 const startingInfectionRate = 0.05;
-const personRadius = 2;
+const personRadius = 4;
 const chanceOfInfection = 0.05;
 const infectionRadius = 1 / 50;
 const healingTime = 500;
 const chanceOfDeath = 0.002;
+
+const maskAdherence = 0.5;
+const maskEffectiveness = 0.1;
 
 const speedMultiplier = 1 / 1000;
 
@@ -38,6 +44,9 @@ function generatePerson(): Person {
 
     xVelocity: speedMultiplier * (Math.random() * 2 - 1),
     yVelocity: speedMultiplier * (Math.random() * 2 - 1),
+
+    mask: Math.random() < maskAdherence,
+    isolating: false,
 
     state: Math.random() < startingInfectionRate ? State.infected : State.susceptible,
     infectionTime: 0,
@@ -60,11 +69,24 @@ function fillStyleForPerson(person: Person): string {
   return style ? style.style : '';
 }
 
+interface XY {
+  x: number;
+  y: number;
+}
+
+function convertPosition(normalisedPosition: XY, width: number, height: number): XY {
+  return {
+    x: personRadius + normalisedPosition.x * (width - 2 * personRadius),
+    y: personRadius + normalisedPosition.y * (height - 2 * personRadius),
+  };
+}
+
 function drawInfectionRing(person: Person, context: CanvasRenderingContext2D, width: number, height: number) {
+  let centre = convertPosition({x: person.xPosition, y: person.yPosition}, width, height);
   context.beginPath();
   context.ellipse(
-    personRadius + person.xPosition * (width - 2 * personRadius),
-    personRadius + person.yPosition * (height - 2 * personRadius),
+    centre.x,
+    centre.y,
     infectionRadius * (width - 2 * personRadius),
     infectionRadius * (height - 2 * personRadius),
     0,
@@ -81,19 +103,23 @@ function drawPerson(person: Person, context: CanvasRenderingContext2D, width: nu
     drawInfectionRing(person, context, width, height);
   }
 
+  let centre = convertPosition({x: person.xPosition, y: person.yPosition}, width, height);
+
   context.beginPath();
-  context.ellipse(
-    personRadius + person.xPosition * (width - 2 * personRadius),
-    personRadius + person.yPosition * (height - 2 * personRadius),
-    2 * personRadius,
-    2 * personRadius,
-    0,
-    0,
-    Math.PI * 2
-  );
+  context.ellipse(centre.x, centre.y, personRadius, personRadius, 0, 0, Math.PI * 2);
   context.closePath();
   context.fillStyle = fillStyleForPerson(person);
   context.fill();
+
+  if (person.mask) {
+    context.beginPath();
+    context.moveTo(centre.x - personRadius, centre.y);
+    context.lineTo(centre.x + personRadius, centre.y);
+    context.lineTo(centre.x, centre.y + personRadius);
+    context.closePath();
+    context.fillStyle = 'blue';
+    context.fill();
+  }
 }
 
 function draw(world: World) {
@@ -138,9 +164,18 @@ function detectCollisions(world: World) {
   const susceptiblePeople = world.people.filter((person) => person.state === State.susceptible);
   infectedPeople.forEach((infectedPerson) => {
     susceptiblePeople.forEach((susceptiblePerson) => {
-      if (distanceBetween(infectedPerson, susceptiblePerson) < infectionRadius && Math.random() < chanceOfInfection) {
-        susceptiblePerson.state = State.infected;
-        susceptiblePerson.infectionTime = world.time;
+      if (distanceBetween(infectedPerson, susceptiblePerson) < infectionRadius) {
+        let myChanceOfInfection = chanceOfInfection;
+        if (susceptiblePerson.mask) {
+          myChanceOfInfection *= 1 - maskEffectiveness;
+        }
+        if (infectedPerson.mask) {
+          myChanceOfInfection *= 1 - maskEffectiveness;
+        }
+        if (Math.random() < myChanceOfInfection) {
+          susceptiblePerson.state = State.infected;
+          susceptiblePerson.infectionTime = world.time;
+        }
       }
     });
   });
